ad013602ad262d5e380734f7cdfa9bcb2e81e2f0
Alex Dryden <adryden3@illinois.edu>
2024-06-12 09:59:27 -0400

    Add and configure factory for media embed options for interfaces

---
+++ src/Service/Media/Ingester/EmbeddedMediaFactory.php
@@ -0,0 +1,16 @@
+<?php
+namespace Mason\Service\Media\Ingester;
+
+use Mason\Media\Ingester\EmbeddedMedia;
+use Omeka\Media\Ingester\Url;
+use Laminas\ServiceManager\Factory\FactoryInterface;
+use Interop\Container\ContainerInterface;
+
+class EmbeddedMediaFactory implements FactoryInterface
+{
+    public function __invoke(ContainerInterface $services, $requestedName, array $options = null)
+    {
+        return new EmbeddedMedia();
+    }
+}
+
