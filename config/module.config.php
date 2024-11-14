f3d219a7ac2542dfc04d5c4dc81015194c7f6766
Alex Dryden <adryden3@illinois.edu>
2024-11-14 11:29:17 -0500

    Cleanup by removing test forms

--- config/module.config.php
+++ config/module.config.php
@@ -21,13 +21,6 @@ return [
 
         ]
     ],
-    'form_elements' => [
-        'factories' => [
-            Form\Element\TeamSelect::class => TeamSelectFactory::class,
-            Form\Element\AllTeamSelect::class => Service\Form\Element\AllTeamSelectFactory::class,
-            Form\Element\AllSiteSelect::class => Service\Form\Element\AllSiteSelectFactory::class,
-        ],
-    ],
     'view_manager' => [
         'template_path_stack' => [
             dirname(__DIR__) . '/view',
